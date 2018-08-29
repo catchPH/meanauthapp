10dd14b19a721d20b9ddc77a488f7943c8667a0c
catchPH <40880680+catchPH@users.noreply.github.com>
2018-08-29 09:55:04 +0800

    Update JWT Token
    Hello world

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const JwtStrategy = require('passport-jwt').Strategy;
+/*const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
 const config = require('../config/database');
@@ -13,6 +13,32 @@ module.exports = function(passport){
 
             if(user) { return done(null, user); }
             else { return done(null, false); }
+        });
+    }));
+}*/
+
+const User = require('../models/user');
+const config = require('../config/database');
+var JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+
+module.exports = function(passport) {
+    var opts = {}
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+    opts.secretOrKey = config.secret;
+    //opts.issuer = 'accounts.examplesoft.com';
+    //opts.audience = 'yoursite.net';
+    passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+        User.findOne({id: jwt_payload.sub}, function(err, user) {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+                // or you could create a new account
+            }
         });
     }));
 }
